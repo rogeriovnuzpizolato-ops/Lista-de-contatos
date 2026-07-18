34f02004c0c0037d8849b9345f970387b16bf261
Rogerio Luiz <rogeriovnuzpizolato@gmail.com>
2026-07-18 11:49:39 -0300

    feat: adicao da funcao assincrona salvarContato no arquivo api.js

--- js/api.js
+++ js/api.js
@@ -7,5 +7,21 @@ const api = {
             alert("erro ao buscar os contatos")
             throw(error)
         }
+    },
+
+    async salvarContato(contato){
+       try {
+        const response = await fetch("http://localhost:3000/contacts",{
+            method: "POST",
+            headers: {
+                "Content-Type" : "application/json"
+            },
+            body: JSON.stringify(contato)
+        })
+        return await response.json()
+       } catch (error) {
+        alert("Erro ao salvar contato")
+        throw error
+       }
     }
 }
