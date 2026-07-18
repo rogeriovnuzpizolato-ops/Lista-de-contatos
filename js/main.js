954731624147e85e338aea51237e2c98da176786
Rogerio Luiz <rogeriovnuzpizolato@gmail.com>
2026-07-18 12:18:37 -0300

    feat: adicao da const e eventListener submit do formulario. criado objeto novoContato contendo nome, telefone, email e favorito.

--- js/main.js
+++ js/main.js
@@ -3,4 +3,19 @@ async function init(){
     ui.renderizarcontatos(contatos)
 }
 
+const form = document.getElementById("contact-form");
+
+form.addEventListener("submit", async(event)=>{
+    event.preventDefault();
+    const novoContato = {
+        name: event.target.name.value,
+        phone: event.target.phone.value,
+        email: event.target.email.value,
+        favorite: false
+    };
+
+    await api.salvarContato(novoContato);
+    init()
+})
+
 init();
